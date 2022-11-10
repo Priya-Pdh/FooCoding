777ecffc8b3aac16b05975b9102dba04775f07e6
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2022-11-10 14:33:03 +0100

    added url path

--- Databases/Week2_part1/nodesql.js
+++ Databases/Week2_part1/nodesql.js
@@ -15,47 +15,62 @@ const app = express();
 // execute will internally call prepare and query
 
 // ANSWER 1: here user takes 'NEPAL' as an country input
-db.execute(
-    'SELECT  ci.name AS capital FROM city ci INNER JOIN country co ON ci.id = co.capital WHERE lower(co.name) = ?', ['nepal'],
+app.get('/countryName/:countryName', (req, res)=>{
+  db.execute(
+    'SELECT  ci.name AS capital FROM city ci INNER JOIN country co ON ci.id = co.capital WHERE lower(co.name) = ?', [req.params.countryName],
     function(err, results, fields) {
       console.log(results); 
       console.log(fields);
-     
+      res.send(results);
     }
-  );
+)
+   
+   } );
 
+
+  
 //ANSWER 2: here user takes 'SOUTHERN EUROPE' as an contient input
-// db.execute(
-//     'SELECT cl.language, co.region FROM countrylanguage cl INNER JOIN country co ON cl.countryCode = co.code  WHERE lower(co.region) = ?', ['southern europe'],
 
-//     function(err, results, fields) {
-//       console.log(results); 
-//       console.log(fields);
-     
-//     }
-//   );
+app.get('/countryLanguage/:continent', (req, res)=>{
+  db.execute(
+    'SELECT DISTINCT cl.language, co.region FROM countrylanguage cl INNER JOIN country co ON cl.countryCode = co.code  WHERE lower(co.region) = ?', [req.params.continent],
+    function(err, results, fields) {
+      console.log(results); 
+      console.log(fields);
+      res.send(results);
+    }
+)
+   
+   } );
+
 
   //ANSWER 3: here user takes 'SWEDISH' as an language input
-// db.execute(
-//     'SELECT count(ci.name) FROM city ci INNER JOIN countrylanguage cl ON ci.countryCode = cl.countryCode WHERE lower(cl.language) = ?', ['swedish'],
 
-//     function(err, results, fields) {
-//       console.log(results); 
-//       console.log(fields);
+  app.get('/numberOfCountry/:language', (req, res)=>{
+    db.execute(
+      'SELECT count(ci.name) FROM city ci INNER JOIN countrylanguage cl ON ci.countryCode = cl.countryCode WHERE lower(cl.language) = ?', [req.params.language],
+      function(err, results, fields) {
+        console.log(results); 
+        console.log(fields);
+        res.send(results);
+      }
+  )
      
-//     }
-//   );
+     } );
 
   //ANSWER 4:
-// db.execute(
-//     'SELECT continent, count(cl.language) AS total_languages_spoken FROM country co INNER JOIN countrylanguage cl ON co.code = cl.countryCode GROUP BY continent;',
 
-//     function(err, results, fields) {
-//       console.log(results); 
-//       console.log(fields);
+  app.get('/continent', (req, res)=>{
+    db.execute(
+      'SELECT DISTINCT continent, count(cl.language) AS total_languages_spoken FROM country co INNER JOIN countrylanguage cl ON co.code = cl.countryCode GROUP BY continent;',
+      function(err, results, fields) {
+        console.log(results); 
+        console.log(fields);
+        res.send(results);
+      }
+  )
      
-//     }
-//   );
+     } );
 
   
 
