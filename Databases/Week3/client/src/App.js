640d261fd6ad5d8d9ca8a6fcea830a744e948cb2
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2022-11-15 20:07:49 +0100

    updates

--- Databases/Week3/client/src/App.js
+++ Databases/Week3/client/src/App.js
@@ -4,11 +4,20 @@ import axios from 'axios'
 function App() {
   const [name, setName] = useState('');
   const[email, setEmail]= useState('');
-  const [user, setUser]=useState([]);
+  const [user, setUser]= useState([]);
 
+ 
+  const [taskList, setTaskList] = useState('');
+  const [dueDate, setDueDate] = useState(null);
+ const[itemList, setItemList]= useState('');
+const [status, setStatus]= useState('');
+
+  const [todos, setTodos] = useState([]);
+
+  
   function addUser(){
-    axios.post('http://localhost:3000/addUser', {
-      name:name,
+    axios.post('http://localhost:3001/addUser', {
+      name:name, 
       email:email,
     }).then(()=> {
       setUser([
@@ -17,17 +26,101 @@ function App() {
     });
   };
 
-  const displarInput= ()=> {
-    console.log(name + email);
+  const getUser= ()=> {
+   axios.get('http://localhost:3001/getUser')
+   .then((response)=> {
+    setUser(response.data);
+   })
   };
+
+  
+
+  function addTodos(){
+    axios.post('http://localhost:3001/addTodos', {
+      
+      taskList:taskList, 
+      due_date: dueDate,
+      item_list: itemList,
+      status: status,
+      
+    }).then(()=> {
+      setTodos([
+        ...todos, { taskList: taskList, due_date: dueDate, item_list: itemList, status:status},
+      ]);
+    });
+  };
+
+
+  // function deleteTodos() {
+  //   axios.post('http://localhost:3001/deleteTodos', {
+
+  //   }).then(()=> {
+  //     setTodsoItemDetails([
+  //       ...todosItemDetails, { item_list: itemList, status:status},
+  //     ]);
+  //   });
+  // };
+  
+
   return (
     <>
     <h1>Users</h1>
-      <label>Name:</label>
+      <label>Name</label>
       <input type='text' onChange={(e)=>{setName(e.target.value)}}/>
-      <label>Email:</label>
+      <label>Email</label>
       <input type='text' onChange={(e)=>{setEmail(e.target.value)}}/>
-      <button onClick={displarInput}>Add User</button>
+      <button onClick={addUser}>Add User</button>
+      <hr/>
+      <div className='userContainer'>
+      <button onClick={getUser}>Show users</button>
+      {user.map((value, key)=> {
+        return <div >
+          <p key ={value.id}>Name: {value.name}</p>
+          <p key ={value.id}>Email: {value.email}</p>
+
+<table>
+  <thead>
+    <tr>
+      <th>Task List</th>
+      <th>Due Date</th>
+      <th>Item List</th>
+      <th>Done</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>
+      <input type='text' onChange={(e)=>{setTaskList(e.target.value)}}/>
+      </td>
+      <td>
+      <input type='text' onChange={(e)=>{setDueDate(e.target.value)}}/>
+      </td>
+      <td>
+      <input type='text' onChange={(e)=>{setItemList(e.target.value)}}/>
+      </td>
+      <td>
+      <input type='text' onChange={(e)=>{setStatus(e.target.value)}}/>
+      </td>
+    </tr>
+  </tbody>
+</table>
+<button onClick={addTodos}>Add Todos Details</button>
+
+{todos.map((value, key)=> {
+        return <div >
+          <p key ={value.id}>Task List: {value.taskList}</p>
+          <p key ={value.id}>Due Date: {value.due_date}</p>
+          <p key ={value.id}>Item List: {value.item_list}</p>
+          <p key ={value.id}>Status: {value.status}</p>
+          </div>})};
+        </div>
+               
+
+      })}
+
+
+      </div>
+     
     </>
     
   );
--- Databases/Week3/server/index.js
+++ Databases/Week3/server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app=express();
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json());
+
 
 app.listen(3001, ()=> {
   console.log('running on port 3001')
@@ -31,7 +34,52 @@ db.connect(function(error){
     const email = req.body.email;
     const sql = `INSERT INTO user(name, email) 
     VALUES (?, ?);`
-    db.execute(sql, [name, email],
+    db.query(sql, [name, email],
+       function(error, result){
+      if(error){
+        console.log(error);
+        return;
+      }
+      res.send(result);
+    });
+  
+  });
+
+  app.get('/getUser', (req,res)=> {
+    db.query('SELECT * FROM user', (error, result)=> {
+        if(error){
+            console.log(error)
+        } else {
+            res.send(result);
+        }
+    })
+  })
+
+
+  app.post('/addTodos', (req,res)=> {
+    // const listID = req.body.listID
+    const taskList = req.body.taskList;
+    const due_date = req.body.due_date;
+    const item_list = req.body.item_list;
+    const status = req.body.status;
+    const sql = `INSERT INTO task(taskList, due_date, item_list, status) 
+    VALUES (?, ?, ?, ?);`
+    db.query(sql, [taskList, due_date, item_list, status],
+       function(error, result){
+      if(error){
+        console.log(error);
+        return;
+      }
+      res.send(result);
+    });
+  
+  });
+  app.post('/addItems', (req,res)=> {
+    const item_list = req.body.item_list;
+    const status = req.body.status;
+    const sql = `INSERT INTO task( item_list, status) 
+    VALUES (?, ?);`
+    db.query(sql, [ item_list, status],
        function(error, result){
       if(error){
         console.log(error);
